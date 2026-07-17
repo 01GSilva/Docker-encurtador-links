1653a3d993b50903ab2a3c5724e18f9fde84c333
Guilherme <01silvaguilherme@gmail.com>
2026-07-17 09:29:01 -0300

    Refatoração para organização do código

--- API.py
+++ API.py
@@ -1,7 +1,7 @@
-from fastapi import fastapi # ferramenta principal
-from pydantic import BaseModel # Biblioteca que o FastAPI usa para validar dados
-import random # Biblioteca para gerar coisas aleatórias
-import string # Biblioteca de constantes uteis, como listas prontas de letras e numeros
+from fastapi import fastapi
+from pydantic import BaseModel
+import random 
+import string 
 from sqlalchemy import create_engine
 from sqlalchemy import sessionmaker
 from sqlalchemy import Column, Integer, String
@@ -11,20 +11,8 @@ import redis
 import json
 from datetime import datetime
 
-app = FastAPI() # criando instancia da aplicação
+# ===== Configuração do Banco de Dados (Postgres) =================================================================================
 
-# ===== "Molde" esperado de como os dados chegam =======================================================================
-class LinkRequest(BaseModel):
-    url_completa: str # "toda requisição precisa ter um campo chamado url_completa do tipo str"
-
-
-# ======================================================================================================================
-def gerar_codigo_curto(tamanho: int=6) -> str:
-    caracteres = string.ascii_letters + string.digits # pool de caracteres possiveis (letras + numeros)
-    return ''.join(random.choices(caracteres, k=tamanho)) # devolve uma lista de caracteres soltos, unificados depois em uma string pelo join
-
-
-# ===== Configuração do Banco de Dados =================================================================================
 DATABASE_URL = "postgresql://usuario:senha@db:5432/encurtador" # endereço do banco de dados
 
 engine = create_engine(DATABASE_URL) # cria o Motor de conexão com o banco
@@ -32,26 +20,15 @@ SessionLocal = sessionmaker(bind=engine) # cria um criador de sessões
 
 Base = declarative_base() # cria uma classe base da qual todas as tabelas vão herdar
 
-
-# ===== Função para abrir e fechar a sessão para liberar a conexão =====================================================
-def get_db():
+def get_db(): # Função para abrir e fechar a sessão para liberar a conexão
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
+# ===== Configuração da Fila (Redis) ============================================================================================
 
-# ===== Define a Tabela "Links" no Banco de dados ======================================================================
-class Link(Base):
-    __tablename__ = 'links' # nome da tabela no banco de dados
-    id = Column(Integer, primary_key=True) # Coluna id da tabela (tipo inteiro e chave primaria)
-    url_completa = Column(String, nullable=False) # Coluna da url completa (Banco impede o valor vazio)
-    codigo_curto = Column(String, nullable=False, unique=True) # Coluna do link encurtado(O proprio banco impede valores repetidos)
-
-
-
-# ===== Configuração e função da Fila ============================================================================================
 redis_client = redis.Redis(host='fila', port=6379, decode_responses=True) # 'fila' como nome de serviço, porta padrão do Redis e decodificação devolvendo str
 
 def enviar_para_fila(codigo_curto: str):
@@ -61,9 +38,34 @@ def enviar_para_fila(codigo_curto: str):
     }
     redis_client.rpush('fila_cliques', json.dumps(dados_clique)) # Insere um item no final da fila (rpush). Converte um dicionario Python em Json (json.dumbs)
 
+# ===== Modelo (Tabela) ======================================================================
+
+class Link(Base):
+    __tablename__ = 'links' # nome da tabela no banco de dados
+
+    id = Column(Integer, primary_key=True) # Coluna id da tabela (tipo inteiro e chave primaria)
+    url_completa = Column(String, nullable=False) # Coluna da url completa (Banco impede o valor vazio)
+    codigo_curto = Column(String, nullable=False, unique=True) # Coluna do link encurtado(O proprio banco impede valores repetidos)
 
 
-# ===== Rota 1  =========================================================================================================
+# ===== Schema de entrada (validade) =======================================================================
+
+class LinkRequest(BaseModel):
+    url_completa: str # "toda requisição precisa ter um campo chamado url_completa do tipo str"
+
+
+# ===== Função auxiliar para gerar código =======================================================================
+
+def gerar_codigo_curto(tamanho: int=6) -> str:
+    caracteres = string.ascii_letters + string.digits # pool de caracteres possiveis (letras + numeros)
+    return ''.join(random.choices(caracteres, k=tamanho)) # devolve uma lista de caracteres soltos, unificados depois em uma string pelo join
+
+# ===== Aplicação =======================================================================
+
+app = FastAPI()
+
+# === Rota 1 ===
+
 @app.post('/encurtar') # Quando chegar uma requisição do tipo POST no endereço /encurtar, execute a função abaixo
 def encurtar_link(request: LinkRequest, db: Session = Depends(get_db())): # Ja recebe os dados validados e prontos para uso (parametro request, tipo LinkRequest)
     link_existente = db.querry(Link).filter_by(url_completa=request.url_completa).first()
@@ -83,8 +85,8 @@ def encurtar_link(request: LinkRequest, db: Session = Depends(get_db())): # Ja r
     db.commit() # grava efetivamente no banco
     return {'link_curto': novo_codigo}
 
+# === Rota 2 ===
 
-# ===== Rota 2 ====================================================================================================
 @app.get('/{codigo_curto}') # Metodo Get, pois está buscando algo que ja existe
 def redirecionar(codigo_curto:str, db:Session = Depends(get_db())):
     link = db.querry(Link).filter_by(codigo_curto=codigo_curto).first() # Busca filtrando pela coluna "codigo_curto"
